108d52f46ea33b1824686b2ef9be9dfc0362fae0
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-01-28 23:30:20 -0700

    some progress

--- main.go
+++ main.go
@@ -1,22 +1,91 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
+	"sort"
 	"strconv"
 	"time"
 
 	qmq "github.com/rqure/qmq/src"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
+
+func QueryPrayerTimesForThisMonth() ([]*qmq.QMQPrayer, error) {
+	var result []*qmq.QMQPrayer
+
+	baseURL := "http://api.aladhan.com/v1/calendarByCity"
+	params := url.Values{}
+	params.Add("city", os.Getenv("CITY"))
+	params.Add("country", os.Getenv("COUNTRY"))
+
+	resp, err := http.Get(fmt.Sprintf("%s?%s", baseURL, params.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var data struct {
+		Data []struct {
+			Timings struct {
+				Fajr    string `json:"Fajr"`
+				Dhuhr   string `json:"Dhuhr"`
+				Asr     string `json:"Asr"`
+				Maghrib string `json:"Maghrib"`
+				Isha    string `json:"Isha"`
+			} `json:"timings"`
+			Date struct {
+				Readable string `json:"readable"`
+			} `json:"date"`
+		} `json:"data"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+
+	for _, day := range data.Data {
+		for prayer, timeStr := range map[string]string{
+			"Fajr":    day.Timings.Fajr,
+			"Dhuhr":   day.Timings.Dhuhr,
+			"Asr":     day.Timings.Asr,
+			"Maghrib": day.Timings.Maghrib,
+			"Isha":    day.Timings.Isha,
+		} {
+			timeParsed, err := time.Parse("02 Jan 2006 15:04 MST", fmt.Sprintf("%s %s +0000", day.Date.Readable, timeStr))
+			if err != nil {
+				continue
+			}
+
+			if time.Now().Before(timeParsed) {
+				result = append(result, &qmq.QMQPrayer{
+					Name: prayer,
+					Time: timestamppb.New(timeParsed),
+				})
+			}
+		}
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].Time.AsTime().Before(result[j].Time.AsTime())
+	})
+
+	return result, nil
+}
 
 func main() {
 	app := qmq.NewQMQApplication("prayer")
 	app.Initialize()
 	defer app.Deinitialize()
 
-	app.AddProducer("prayer:time-exchange").Initialize(500)
-	app.AddProducer("prayer:adhan-exchange").Initialize(1)
-	app.AddConsumer("prayer:time-queue").Initialize()
+	app.AddProducer("prayer:time:exchange").Initialize(500)
+	app.AddProducer("prayer:adhan:exchange").Initialize(1)
+	app.AddConsumer("prayer:time:queue").Initialize()
 
 	tickRateMs, err := strconv.Atoi(os.Getenv("TICK_RATE_MS"))
 	if err != nil {
@@ -33,7 +102,44 @@ func main() {
 			app.Logger().Advise("SIGINT received")
 			return
 		case <-ticker.C:
+			next_prayer := &qmq.QMQPrayer{}
+			popped := app.Consumer("prayer:time-queue").Pop(next_prayer)
+			if popped != nil {
+				if time.Now().After(next_prayer.Time.AsTime()) {
+					app.Logger().Advise(fmt.Sprintf("It is now time for: %s", next_prayer.Name))
 
+					audioFiles := []string{
+						"/app/audio/adhan/adhan-0.mp3",
+						"/app/audio/adhan/adhan-1.mp3",
+						"/app/audio/adhan/adhan-2.mp3",
+						"/app/audio/adhan/adhan-3.mp3",
+						"/app/audio/adhan/adhan-5.mp3",
+						"/app/audio/adhan/adhan-wahhab.mp3"}
+
+					if next_prayer.Name == "Fajr" {
+						audioFiles = []string{"/app/audio/adhan/fajr-1.mp3"}
+					}
+
+					randomIndex := rand.Intn(len(audioFiles))
+					audioFile := audioFiles[randomIndex]
+
+					app.Producer("prayer:adhan:exchange").Push(&qmq.QMQAudioRequest{
+						Filename: audioFile,
+					})
+					popped.Ack()
+				} else {
+					popped.Dispose()
+				}
+			} else {
+				prayers, err := QueryPrayerTimesForThisMonth()
+				if err != nil {
+					app.Logger().Error(fmt.Sprintf("Failed to query prayer times: %v", err))
+				} else {
+					for _, prayer := range prayers {
+						app.Producer("prayer:time:exchange").Push(prayer)
+					}
+				}
+			}
 		}
 	}
 }
